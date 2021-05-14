6eb27bcf3b6d09f8b010cc3be120ec288e6351d2
Lucas Avanzi <lucasavanzi97@gmail.com>
2021-05-14 07:58:08 -0400

    Encriptando  a senha antes de salvar

--- src/app/models/User.js
+++ src/app/models/User.js
@@ -1,5 +1,6 @@
 const Sequelize = require('sequelize');
 const { Model } = require('sequelize');
+const bcrypt = require('bcryptjs');
 
 class User extends Model {
   static init(sequelize) {
@@ -17,7 +18,17 @@ class User extends Model {
       },
     );
 
+    this.addHook('beforeSave', async (user) => {
+      if (user.password) {
+        user.password = await bcrypt.hash(user.password, 8);
+      }
+    });
+
     return this;
+  }
+
+  checkPassword(password) {
+    return bcrypt.compare(password, this.password);
   }
 }
 
