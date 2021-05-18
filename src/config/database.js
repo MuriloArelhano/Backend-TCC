82b56539057e98a5b428f7d47f356629aa5fb955
Lucas Avanzi <lucasavanzi97@gmail.com>
2021-05-18 07:34:28 -0400

    Alterando a conexão com o banco de dados

--- src/config/database.js
+++ src/config/database.js
@@ -1,9 +1,12 @@
 module.exports = {
   dialect: 'postgres',
-  host: 'localhost',
-  username: 'postgres',
-  password: 'postgres',
-  database: 'db_devrel',
+  dialectOptions: {
+    ssl: { require: true, rejectUnauthorized: false },
+  },
+  host: 'ec2-3-234-85-177.compute-1.amazonaws.com',
+  username: 'rrsobbyoqjlbje',
+  password: '58f1671268656f88620f5eddd5b4ffe425cfafeb796794a6c38e1603c8926a1d',
+  database: 'dc84e1t7tnnike',
   define: {
     timestamps: true,
     underscored: true,
