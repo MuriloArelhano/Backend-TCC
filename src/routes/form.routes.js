1057faad40babd736896b3c6d9fab7e70f3bc1b5
Lucas Avanzi <lucasavanzi97@gmail.com>
2021-06-09 13:37:47 -0400

    incluindo o middleware de autenticação para poder criar o form

--- src/routes/form.routes.js
+++ src/routes/form.routes.js
@@ -1,9 +1,11 @@
 const { Router } = require('express');
 // controllers
 const FormController = require('../app/controllers/FormController');
+// middlewares
+const authMiddleware = require('../app/middlewares/auth');
 
 const formRouter = Router();
 
-formRouter.post('/', FormController.create);
+formRouter.post('/', authMiddleware, FormController.create);
 
 module.exports = formRouter;
